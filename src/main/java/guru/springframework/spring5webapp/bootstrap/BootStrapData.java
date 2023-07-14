9a4824f6b4e0e222c2e94bcd19a1415390bab169
Dominic Ngoetjana <dngoetjana@iqbusiness.net>
2023-07-14 19:22:51 +0200

    Add jpa-entities managed through command line runner

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,62 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repos.AuthorRepo;
+import guru.springframework.spring5webapp.repos.BookRepo;
+import guru.springframework.spring5webapp.repos.PublisherRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepo authorRepo;
+    private final BookRepo bookRepo;
+    private final PublisherRepo publisherRepo;
+
+    public BootStrapData(AuthorRepo authorRepo, BookRepo bookRepo, PublisherRepo publisherRepo) {
+        this.authorRepo = authorRepo;
+        this.bookRepo = bookRepo;
+        this.publisherRepo = publisherRepo;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
+
+        Publisher books2world = new Publisher("Books2World", "100 Street Lane", "New Byork", "Cali", "3003003");
+        publisherRepo.save(books2world);
+
+        System.out.println("Publisher Count: " + publisherRepo.count());
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        ddd.setPublisher(books2world);
+        books2world.getBooks().add(ddd);
+
+        authorRepo.save(eric);
+        bookRepo.save(ddd);
+        publisherRepo.save(books2world);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "13152548646");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        noEJB.setPublisher(books2world);
+        books2world.getBooks().add(noEJB);
+
+        authorRepo.save(rod);
+        bookRepo.save(noEJB);
+        publisherRepo.save(books2world);
+
+        System.out.println("Number of Books: " + bookRepo.count());
+        System.out.println("Publisher Number of Books: " + books2world.getBooks().size());
+    }
+}
